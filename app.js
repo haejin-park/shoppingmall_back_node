394f3a81c19824a2f2f9f00faf0059ce150aaeeb
haejin <luckyhaejin1126@gmail.com>
2024-04-16 19:59:37 +0900

    프록시 사용하여 배포하기위해 app.js 수정, 프론트 요청 허가되도록 cors.config (cors 코드 중복 되지않게 수정, helmet삭제)

--- app.js
+++ app.js
@@ -1,37 +1,23 @@
-const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const hpp = require('hpp');
-const helmet = require('helmet');
 require("dotenv").config();
 const indexRouter = require('./routes/index');
-
+const express = require('express');
 const app = express();
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
-const frontURL = process.env.NODE_ENV === 'production' 
-? `${process.env.FRONT_PROD_URL}` : `${process.env.FRONT_DEV_URL}`;
 
 if(process.env.NODE_ENV === 'production'){
     app.use(morgan('combined'));
     app.use(hpp());
-    app.use(helmet());
-    app.use(cors({
-        origin: frontURL,
-        credentials: true,
-    }));
 } else {
     app.use(morgan('dev'));
-    app.use(cors({
-        origin: frontURL,
-        credentials: true,
-    }));
 }
 
-
+app.use(cors());
 app.use('/api',indexRouter);
 
 const mongoURI =  process.env.NODE_ENV === 'production' 
@@ -42,6 +28,6 @@ mongoose
     .then(() => console.log("mongoose connected"))
     .catch((error) => console.error(error));
 
-app.listen(process.env.PORT || 5001, () => {
+app.listen(5001, () => {
     console.log("server on");
 });

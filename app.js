c37b5f6c6153cb8dc9ebdc2099a8f5a269bad8df
haejin <luckyhaejin1126@gmail.com>
2024-04-16 12:54:48 +0900

    morgan(로깅 편리하기), hpp, helmet(공격 방어, 보안 강화) 설치

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
+const hpp = require('hpp');
+const helmet = require('helmet');
 require("dotenv").config();
 const indexRouter = require('./routes/index');
 
@@ -11,10 +14,24 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
 const frontURL = process.env.NODE_ENV === 'production' 
 ? `${process.env.FRONT_PROD_URL}` : `${process.env.FRONT_DEV_URL}`;
-app.use(cors({
-    origin: frontURL,
-    credentials: true,
-}));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(morgan('combined'));
+    app.use(hpp());
+    app.use(helmet());
+    app.use(cors({
+        origin: frontURL,
+        credentials: true,
+    }));
+} else {
+    app.use(morgan('dev'));
+    app.use(cors({
+        origin: frontURL,
+        credentials: true,
+    }));
+}
+
+
 app.use('/api',indexRouter);
 
 const mongoURI =  process.env.NODE_ENV === 'production' 
